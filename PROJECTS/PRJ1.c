15857ec386e2f55d1f302c57ec95cffd16fd75f3
AnkurkrSrivastava <ankur821305@gmail.com>
2025-05-05 21:40:00 +0530

    little changes in this project

--- PROJECTS/PRJ1.c
+++ PROJECTS/PRJ1.c
@@ -2,31 +2,42 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main() {
+int main()
+{
     // Seed the random number generator with current time
-    srand(time(0));  
+    srand(time(0));
 
     // Generate a random number between 0 and 100
-    int random = rand() % 101;  // rand() % 101 gives numbers from 0 to 100
+    int random = rand() % 101; // rand() % 101 gives numbers from 0 to 100
     int guess;
     int attempts = 0;
     printf("Welcome to the Guessing Game!\n");
-    printf("Guess a number between 0 and 100: ");
-    scanf("%d", &guess);
-    while (guess != random)
+
+    while (1)
     {
-        if(guess < random) {
-            printf("Too low! Try again:\n");
-        } else {
-            printf("Too high! Try again:\n");
-        }
-        
-        printf("Guess a number again between 0 and 100:\n ");
+        printf("Guess a number between 0 and 100:\n");
         scanf("%d", &guess);
+        if (guess < 0 || guess > 100)
+        {
+            printf("Invalid input! Please enter a number between 0 and 100:\n");
+            continue; // Skip the rest of the loop and ask for input again
+        }
         attempts++;
-        
+        if (guess < random)
+        {
+            printf("Low number! Try little higher again:\n");
+        }
+        else if (guess > random)
+        {
+            printf("High number! Try little lower again:\n");
+        }
+        else
+        {
+            printf("Congratulations! You guessed the number %d in %d attempts\n", random, attempts);
+            break;
+        }
+
     }
-    printf("Congratulations! You guessed the number %d in %d attempts\n", random ,attempts);
-    
+
     return 0;
 }
